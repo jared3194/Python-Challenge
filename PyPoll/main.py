7916f610311db29d7cd3d7340e94f5d99204b82d
Jared Nieves <jared3194@gmail.com>
2021-01-13 23:05:46 -0500

    Made final edits to the script to print output to the terminal and write the correct results to the output file.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,30 +13,46 @@ header = next(reader)
 
 Total_Votes = 0
 Candidates = []
-Unique_Candidate = set()
 Winner = 0
 Winning_Candidate = ""
 percentage = []
 Vote_Count = []
 percent = 0
-Unique_Candidate_List = ["O'Tooley", "Khan", "Correy", "Li"]
+Unique_Candidate_List = []
+
+# Create Output file
+analyzed_path = "Analysis/Polling_Analysis.txt"
+Outfile = open(analyzed_path, 'w+')
 
 # Caluclate the total number of votes
-
 
 for row in reader:
     Total_Votes += 1
 
     # Determine the complete list of unique candidates
-
-    Unique_Candidate.add(row[2])
-
     # Add candidates to list
 
     Candidates.append(row[2])
 
+# Begin writing to the output file and printing to terminal
 
-for name in Unique_Candidate:
+Outfile.write("Election Results" + "\n")
+Outfile.write("-------------------------" + "\n")
+Outfile.write("Total Votes: " + str(Total_Votes) + "\n")
+Outfile.write("-------------------------" + "\n")
+
+print(
+"Election Results" + "\n"
+"-------------------------" + "\n"
+"Total Votes: " + str(Total_Votes) + "\n"
+"-------------------------" + "\n"
+)
+    
+for row in Candidates: 
+    if row not in Unique_Candidate_List:
+        Unique_Candidate_List.append(row)
+
+for name in Unique_Candidate_List:
     count = 0
     for row in Candidates:
         if name == row:
@@ -45,25 +61,32 @@ for name in Unique_Candidate:
         if count > Winner:
             Winner = count
             Winning_Candidate = name
-
-    # Put data to the lists
+    
+    i = Unique_Candidate_List.index(name)
+    
+# Put data to the lists
     percentage.append(percent)
     Vote_Count.append(count) 
+    
+# Write/print this part of the out file inside the loop to loop through the candidate info using the index
+    Outfile.write(f"{Unique_Candidate_List[i]}: {percentage[i]:.3f}% ({Vote_Count[i]})\n")
+    print(f"{Unique_Candidate_List[i]}: {percentage[i]:.3f}% ({Vote_Count[i]})\n")
 
-    i = Unique_Candidate_List.index(name)
-
-
-analyzed_path = "Analysis/Polling_Analysis.txt"
-Outfile = open(analyzed_path, 'w+')
-
-Outfile.write("Election Results" + "\n")
-Outfile.write("-------------------------" + "\n")
-Outfile.write("Total Votes: " + str(Total_Votes) + "\n")
-Outfile.write("-------------------------" + "\n")
-Outfile.write({Unique_Candidate_List[i]} + ":" + str(percentage[i]) + "%" + str(Vote_Count[i]) + "\n")
+# Write/print remaining portions of the output outside of the loop to avoid duplicating outputs
+  
 Outfile.write("-------------------------" + "\n")
 Outfile.write("Winner: " + Winning_Candidate + "\n")
 Outfile.write("-------------------------")
+
+print(
+"-------------------------" + "\n"
+"Winner: " + Winning_Candidate + "\n"
+"-------------------------"
+)
+
+
+
+
 
 
     
